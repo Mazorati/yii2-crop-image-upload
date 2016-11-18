61dcd9e635bc9c20f94dc083514d339852008912
Alex iMac <mz@mac7.ru>
2016-11-19 00:09:21 +0300

    fix

--- CropImageUpload.php
+++ CropImageUpload.php
@@ -19,123 +19,121 @@ use yii\widgets\InputWidget;
  */
 class CropImageUpload extends InputWidget
 {
-	/**
-	 * @var array the options for the jCrop plugin.
-	 * Please refer to the jCrop Web page for possible options.
-	 * @see http://deepliquid.com/content/Jcrop_Manual.html
-	 */
-	public $clientOptions = ['boxWidth' => 450, 'boxHeight' => 400];
+    /**
+     * @var array the options for the jCrop plugin.
+     * Please refer to the jCrop Web page for possible options.
+     * @see http://deepliquid.com/content/Jcrop_Manual.html
+     */
+    public $clientOptions = ['boxWidth' => 450, 'boxHeight' => 400];
 
-	/**
-	 * @var string crop ratio
-	 * format is width:height where width and height are both floats
-	 * if has model, will be got from CropImageBehavior
-	 */
-	public $ratio;
+    /**
+     * @var string crop ratio
+     * format is width:height where width and height are both floats
+     * if has model, will be got from CropImageBehavior
+     */
+    public $ratio;
 
-	/**
-	 * @var string attribute name storing crop value or crop value itself if no model
-	 * if has model, will be got from CropImageBehavior
-	 * crop value has topLeftX-topLeftY-width-height format where all variables are float
-	 * all coordinates are in percents of corresponded image dimension
-	 */
-	public $crop_field;
+    /**
+     * @var string attribute name storing crop value or crop value itself if no model
+     * if has model, will be got from CropImageBehavior
+     * crop value has topLeftX-topLeftY-width-height format where all variables are float
+     * all coordinates are in percents of corresponded image dimension
+     */
+    public $crop_field;
 
-	/**
-	 * @var string crop value
-	 * if has model, will be got from $crop_field of model
-	 * crop value has topLeftX-topLeftY-width-height format where all variables are float
-	 * all coordinates are in percents of corresponded image dimension
-	 */
-	public $crop_value;
+    /**
+     * @var string crop value
+     * if has model, will be got from $crop_field of model
+     * crop value has topLeftX-topLeftY-width-height format where all variables are float
+     * all coordinates are in percents of corresponded image dimension
+     */
+    public $crop_value;
 
-	/**
-	 * @var string css class of container that stores image crop
-	 */
-	public $crop_class = 'crop_medium';
-
-
-
-	/**
-	 * @var string url where uploaded files are stored
-	 * if empty and has model, will be got from CropImageBehavior
-	 */
-	public $url;
+    /**
+     * @var string css class of container that stores image crop
+     */
+    public $crop_class = 'crop_medium';
 
 
-	/**
-	 * @inheritdoc
-	 */
-	public function run()
-	{
-		$jsOptions = [
-			'clientOptions' => $this->clientOptions,
-			'crop_class' => $this->crop_class,
-		];
 
-		if ($this->hasModel()) {
-			echo Html::activeInput('file', $this->model, $this->attribute, $this->options);
-
-			$crops = null;
-			foreach ($this->model->getBehaviors() as $beh) {
-				if (!empty($beh->attribute) && $beh->attribute == $this->attribute && $beh instanceof CropImageUploadBehavior) {
-					$crops = $beh->getConfigurations();
-					$this->url = $beh->url;
-					break;
-				}
-			}
-
-			if (!$crops)
-				throw new InvalidConfigException("CropImageUploadBehavior is not found for {$this->attribute} attribute");
-
-			$jsOptions['crops'] = [];
-			$input_name = Html::getInputName($this->model, $this->attribute);
-			$input_id = Html::getInputId($this->model, $this->attribute);
-
-			echo Html::hiddenInput($input_name . '[file]', Html::getAttributeValue($this->model, $this->attribute), ['id' => $input_id . '_image']);
-
-			foreach ($crops as $ind => $crop) {
-				$crop_id = $input_id . '_crop' . $ind;
-				echo Html::hiddenInput($input_name . '[' . $ind . ']', $crop['value'] === false ? '-' : $crop['value'], ['id' => $crop_id]);
-
-				$jsOptions['crops'][] = [
-					'input_id' => $crop_id,
-					'ratio' => $crop['ratio'],
-					'image' => $crop['image'],
-				];
-			}
-
-		} else {
-			echo Html::fileInput($this->name, $this->value, $this->options);
-
-			$crop_id = (isset($this->options['id']) ? $this->options['id'] : ($this->name . '_id')) . '_' . $this->crop_field;;
-			echo Html::hiddenInput($this->crop_field, $this->crop_value, ['id' => $crop_id]);
-
-			$jsOptions['crops'][] = [
-				'input_id' => $crop_id,
-				'ratio' => $this->ratio,
-			];
-		}
-
-		if ($this->url)
-			$this->url = \Yii::getAlias($this->url);
-
-		$jsOptions['url'] = $this->url;
+    /**
+     * @var string url where uploaded files are stored
+     * if empty and has model, will be got from CropImageBehavior
+     */
+    public $url;
 
 
-		$this->registerPlugin($jsOptions);
-	}
+    /**
+     * @inheritdoc
+     */
+    public function run()
+    {
+        $jsOptions = [
+            'clientOptions' => $this->clientOptions,
+            'crop_class' => $this->crop_class,
+        ];
 
-	/**
-	 * Registers jCrop
-	 */
-	protected function registerPlugin($options)
-	{
-		$view = $this->getView();
-		CropImageUploadAsset::register($view);
+        if ($this->hasModel()) {
+            echo Html::activeInput('file', $this->model, $this->attribute, $this->options);
 
-		$id = $this->options['id'];
+            $crops = [
+                [
+                    'cropped_field' => 'photo_cropped',
+                    'crop_field' => 'photo_crop',
+                    'ratio' => 9/5,
+                    'value' => null,
+                    'image' => null
+                ]
+            ];
 
-		$view->registerJs("jQuery('#{$id}').cropImageUpload(".json_encode($options).");");
-	}
+
+            $jsOptions['crops'] = [];
+            $input_name = Html::getInputName($this->model, $this->attribute);
+            $input_id = Html::getInputId($this->model, $this->attribute);
+
+            //echo Html::hiddenInput($input_name . '[file]', Html::getAttributeValue($this->model, $this->attribute), ['id' => $input_id . '_image']);
+
+            echo Html::hiddenInput( Html::getInputName($this->model, 'crop_'.$this->id), '-', ['id' => $input_id . '_crop' . $this->id]);
+
+            $jsOptions['crops'][] = [
+                'input_id' => $input_id . '_crop' . $this->id,
+                'ratio' => 9/5,
+                'image' => null,
+            ];
+
+
+
+        } else {
+            echo Html::fileInput($this->name, $this->value, $this->options);
+
+            $crop_id = (isset($this->options['id']) ? $this->options['id'] : ($this->name . '_id')) . '_' . $this->crop_field;;
+            echo Html::hiddenInput($this->crop_field, $this->crop_value, ['id' => $crop_id]);
+
+            $jsOptions['crops'][] = [
+                'input_id' => $crop_id,
+                'ratio' => $this->ratio,
+            ];
+        }
+
+        if ($this->url)
+            $this->url = \Yii::getAlias($this->url);
+
+        $jsOptions['url'] = $this->url;
+
+
+        $this->registerPlugin($jsOptions);
+    }
+
+    /**
+     * Registers jCrop
+     */
+    protected function registerPlugin($options)
+    {
+        $view = $this->getView();
+        CropImageUploadAsset::register($view);
+
+        $id = $this->options['id'];
+
+        $view->registerJs("jQuery('#{$id}').cropImageUpload(".json_encode($options).");");
+    }
 } 
